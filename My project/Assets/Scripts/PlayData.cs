ea4ea99c2caaf3f34b41b6fce67ac9edbda4e6c6
Rhon07 <116782153+Rhon07@users.noreply.github.com>
2022-12-05 12:26:54 +0800

    Latest

---
+++ My project/Assets/Scripts/PlayData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayData : MonoBehaviour
+{
+   public void clickpass()
+    {
+        SceneManager.LoadScene("secondScene");
+    }
+}
